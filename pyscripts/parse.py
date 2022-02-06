1f13b263b7c926b4e8764a90dfa404efb81fb123
Radon10043 <772577952@qq.com>
2022-02-06 14:48:55 +0800

    Modify getInfo function

--- pyscripts/parse.py
+++ pyscripts/parse.py
@@ -7,55 +7,83 @@ import queue
 import networkx as nx
 
 
-def getInfo(path: str):
+def getInfo(dotPath: str, taintPath: str):
     """获取所有dot文件路径, 调用函数信息和污点源
 
     Parameters
     ----------
-    path : str
+    dotPath : str
         dot文件夹的路径
+    taintPath : str
+        污点源txt文件的路径
+
+    Returns
+    -------
+    [type]
+        [description]
 
     Notes
     -----
     [description]
     """
-    if not os.path.exists(path):
-        print("文件夹" + path + "不存在.", file=sys.stderr)
+    if not os.path.exists(dotPath):
+        print("文件夹" + dotPath + "不存在.", file=sys.stderr)
         return
-    if not os.path.isdir(path):
-        print(path + "不是文件夹, 分析停止.", file=sys.stderr)
+    if not os.path.isdir(dotPath):
+        print(dotPath + "不是文件夹, 分析停止.", file=sys.stderr)
         return
 
+    if not os.path.exists(taintPath):
+        print("文件" + taintPath + "不存在.", file=sys.stderr)
+        return
+    if not os.path.splitext(taintPath)[1] == ".txt":
+        print(taintPath + "不是txt文件.", file=sys.stderr)
+        return
+
+    # 获取所有dot图的路径
     dotfileList = list()  # 所有dot图的路径
-    for file in os.listdir(path):
-        if os.path.splitext(file)[1] != ".dot": # 跳过非dot文件
+    for file in os.listdir(dotPath):
+        if os.path.splitext(file)[1] != ".dot":  # 跳过非dot文件
             continue
-        dotfileList.append(os.path.join(path, file))
+        dotfileList.append(os.path.join(dotPath, file))
 
+    # 获取调用信息
     callDict = dict()  # <文件名与行号, <被调用的函数名, <参数, 变量>>>
-    lines = open(os.path.join(path, "linecalls.txt")).readlines()
+    lines = open(os.path.join(dotPath, "linecalls.txt")).readlines()
     for line in lines:
         line = line.rstrip("\n")
-        loc, calledF, pvs = line.split("-") # 文件名与行号, 被调用的函数, 参数与变量列表
+        loc, calledF, pvs = line.split("-")  # 文件名与行号, 被调用的函数, 参数与变量列表
 
-        callDict[loc] = dict()
-        callDict[loc][calledF] = dict()
+        if not loc in callDict.keys():
+            callDict[loc] = dict()
+        if not calledF in callDict[loc].keys():
+            callDict[loc][calledF] = dict()
 
-        pvs = pvs.split(",")    # 将parameter与variable写入字典
+        pvs = pvs.split(",")  # 将parameter与variable写入字典
         for pv in pvs:
             try:
-                p,v = pv.split(":")
-                callDict[loc][calledF][p] = v
+                p, v = pv.split(":")
+
+                if not p in callDict[loc][calledF].keys():
+                    callDict[loc][calledF][p] = v.split("|")
+                else:
+                    callDict[loc][calledF][p].append(v)
+
             except:
                 continue
 
-    # TODO: 污点源队列
+    # 获取污点源队列
+    taintList = open(taintPath).readlines()
+    taintQueue = queue.Queue()
+    for taint in taintList:
+        taintQueue.put(taint.rstrip("\n"))
 
-    return dotfileList, callDict
+    return dotfileList, callDict, taintQueue
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", help="存储dot图的文件夹", required=True)
+    parser.add_argument("-d", "--dots", help="存储dot图的文件夹", required=True)
+    parser.add_argument("-t", "--taint", help="存储污点源信息的txt文件", required=True)
     args = parser.parse_args()
-    dotfileList, callDict = getInfo(args.path)
+    dotfileList, callDict, taintQueue = getInfo(args.dots, args.taint)
